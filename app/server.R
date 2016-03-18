2e166f145a0e36f7aaa8b81afdd3fde727939f08
Molly Lewis <mll@stanford.edu>
2016-03-17 18:35:48 -0700

    added lots of features to app

--- app/server.R
+++ app/server.R
@@ -1,10 +1,11 @@
 library(ggvis)
 library(dplyr)
+library(shinyapps)
+library(broom)
+library(R.utils)
 
 # Set up handles to database tables on app start
 d <- read.csv("data/langLearnVar_data_clean.csv")
-#d <- read.csv("../data/langLearnVar_data_clean.csv")
-# (when running locally)
 
 # filter variables
 #"Language" = "stock"                         
@@ -32,55 +33,122 @@ shinyServer(function(input, output, session) {
     d[!is.na(d[,input$xvar])  & !is.na(d[,input$yvar]),]
   })
   
-  # A reactive expression with the ggvis plot
+  # model
+  mod <- reactive({
+    if (input$model == 1) { # simple linear regression
+      lm(as.formula(paste(input$yvar," ~ ",input$xvar)), data = d.sub())
+      
+    } else if (input$model == 2) { # random intercept of country
+      lmer(as.formula(paste(input$yvar,  "~" , input$xvar,  "+ (1|native.country)")), 
+           d.sub(), control=lmerControl(optimizer = "bobyqa"))
+      
+    } else if (input$model == 3) { # random intercept of family
+      lmer(as.formula(paste(input$yvar,  "~" , input$xvar,  "+ (1|lang.family)")), 
+           d.sub(), control=lmerControl(optimizer = "bobyqa"))
+      
+    } else if (input$model == 4) { # both random interecepts
+      lmer(as.formula(paste(input$yvar,  "~" , input$xvar,  "+ (1|lang.family) + (1|native.country)")), 
+           d.sub(), control=lmerControl(optimizer = "bobyqa"))
+      
+    } else if (input$model == 5) {  # random intercept and slope of family, intercept of country
+      lmer(as.formula(paste(input$yvar,  "~" , input$xvar,  "+(", input$xvar, "|lang.family) +
+                                (1|native.country)")), d.sub(), control=lmerControl(optimizer = "bobyqa"))
+      
+    } # full model doesn't have enough levels
+  })
+  
+  # this is necessary to create the line fit on the plot because ggvis doesn't have an abline equivalent
+  data_line <- reactive({
+    xmin = min(d.sub()[,input$xvar])
+    xmax = max(d.sub()[,input$xvar])
+    
+    line.params = tidy(mod())%>%
+      filter(term == input$xvar | term == "(Intercept)") %>%
+      select(term, estimate) %>%
+      spread(term, estimate) %>%
+      rename_(m = input$xvar) %>%
+      rename(b = `(Intercept)`)
+    
+    m = line.params$m 
+    b = line.params$b
+             
+    data.frame(
+      x_rng = c(xmin, xmax), 
+      y_rng = c(m*xmin + b, m*xmax + b))     
+  }) 
+  
+  # function for generating tooltip text
+  language_tooltip <- function(x) {
+    if (is.null(x)) return(NULL)
+    if (is.null(x$language)) return(NULL)
+    
+    # Pick out the language with this ID
+    all_d <- isolate(d.sub())
+    lang <- all_d[all_d$language == x$language, ]
+    lang = lang %>% 
+            filter(!is.na(language)) %>%
+          slice(1)
+
+    paste0("<b>", capitalize(lang$language), "</b><br>",
+           lang$lang.family, "<br>",
+           lang$native.country)
+  }
+  
+  
+  # scatter plot
   vis <- reactive({
-    # Labels for axes
+    # axes labels
     xvar_name <- names(axis_vars)[axis_vars == input$xvar]
     yvar_name <- names(axis_vars)[axis_vars == input$yvar]
     xvar <- prop("x", as.symbol(input$xvar))
     yvar <- prop("y", as.symbol(input$yvar))
     
-    d.sub %>%
+    
+    d.sub() %>%
       ggvis(x = xvar, y = yvar) %>%
-      layer_model_predictions(model = "lm", se = TRUE) %>%
+      #layer_model_predictions(model = "lm", se = TRUE) %>%
       layer_points(size := 50, size.hover := 200,
-        fillOpacity := 0.7, fill = ~lang.family, fillOpacity.hover := 0.5, key := ~language) %>%
-      add_tooltip(function(df) df$language) %>%
+        fillOpacity := 0.7, fill = ~lang.family, 
+        fillOpacity.hover := 0.5, key := ~language) %>%
+      layer_paths(x = ~x_rng, y = ~y_rng, stroke := "black",strokeWidth := 5, data = data_line) %>%
+      #add_tooltip(function(df) df$language) %>%
+      add_tooltip(language_tooltip, "hover") %>%
       add_axis("x", title = xvar_name, title_offset = 50) %>%
       add_axis("y", title = yvar_name, title_offset = 50) %>% 
       set_options(width = 500, height = 500) %>%
       hide_legend('fill')
   })
-  
   vis %>% bind_shiny("scatterPlot")
   
-  # Generate a summary of the dataset
-  output$summary <- renderPrint({
-    # Lables for axes
-    xvar_name <- names(axis_vars)[axis_vars == input$xvar]
-    yvar_name <- names(axis_vars)[axis_vars == input$yvar]
-    xvar <- prop("x", as.symbol(input$xvar))
-    yvar <- prop("y", as.symbol(input$yvar))
-    yy = input$yvar
-    #m = d[!is.na(d[,input$xvar])  & !is.na(d[,input$yvar]),]
-    m = d.sub %>% select(yy)
-    summary(m)
-   #lmer(input$yvar ~ input$xvar + (input$xvar|lang.family) +
-              # (1|native.country), m)
-  })
-  
-  output$mapPlot <- renderPlot({
-    d.sub() %>%
-      ggplot() +   
-        borders("world", colour="gray68", fill="gray68") +
-        geom_point(aes(x = lon, y = lat), size = 3) +
-        ggtitle("Geographical Distribution of Languages") + 
-      mapTheme
-  }, height = 275)
+  # summary of model
+  output$summary <- renderPrint({ 
+    summary(mod())
+  }, width = "600")
 
+  # number of languages
   output$n_languages<- renderText({ 
         nrow(d.sub())
     })
+  
+  # number of families
+  output$n_families<- renderText({ 
+    m = droplevels(d.sub()$lang.family)
+    length(summary(m))
+  })
+  
+  # map plot
+  output$mapPlot <- renderPlot({
+    d.sub() %>%
+      ggplot() +   
+      borders("world", colour="gray68", fill="gray68") +
+      geom_point(aes(x = lon, y = lat, color = lang.family), size = 3) +
+      mapTheme +
+      theme(legend.position="none") 
+     #theme(legend.position="bottom", legend.title=element_blank())
+  }, height = 275)
+  
 })
+
+
 
 #shinyapps::deployApp('/Documents/GRADUATE_SCHOOL/Projects/langLearnVar/app/',account = "mlewis", appName="lhnn")
--- app/ui.R
+++ app/ui.R
@@ -2,34 +2,66 @@ library(ggvis)
 library(rworldmap)
 library(tidyr)
 library(ggplot2)
+library(shinythemes)
 library(lme4)
 
 shinyUI(fluidPage(
-  titlePanel("Ling. Niche Hypothesis Aggregated Dataset"),
+  theme = shinytheme("spacelab"),
+  
+  titlePanel("Linguistic Niche Browser"),
+  p("Explore an aggregation of datasets that test relationships between features of the environment and elements of language.
+    This browser is supplemental material to",
+    a("Lewis and Frank (under review, CogSci 2016).", 
+      href = "hhttps://langcog.stanford.edu/papers_new/lewis-2016-underrev.pdf", target = "_blank")),
+  br(),
+  
   fluidRow(
     column(6,
       wellPanel(
-        selectInput("xvar", "X-axis variable", axis_vars, selected = "pop_log"),
-        selectInput("yvar", "Y-axis variable", axis_vars, selected = "complexity.bias"),
+        selectInput("xvar", "Predictor", axis_vars, selected = "pop_log"),
+        selectInput("yvar", "Outcome", axis_vars, selected = "complexity.bias"),
         tags$small(paste0(
-          "Color on the scatter plot indicates language family. Line shows simple linear regression."
+          "Color on the scatter plot indicates language family.
+          Hover over points to identifiy individual languages."
         ))
       ),
-      wellPanel(
-        span("Number of languages:",
-             textOutput("n_languages")
-        )
-      ),
-      wellPanel(
-        span("Model Fits",
-             textOutput("xx_languages")
-        )
-      ),
-      plotOutput("mapPlot")
+
+      wellPanel(radioButtons("model", "Model fit:",
+                                     selected = 1,                   
+                                     c("simple linear regression" = 1,
+                                       "random intercept of country" = 2,
+                                       "random intercept of family" = 3,
+                                       "random intercepts of country and family" = 4,
+                                       "random intercept and slope of family, intercept of country" = 5),
+                                    width = '500px')
+      )
+      
     ),
     column(6, 
-      ggvisOutput("scatterPlot")
-      #verbatimTextOutput("summary")
+    tabsetPanel(
+      tabPanel("Predictions Plot",
+                 br(),
+                 h4(""),
+                 div(style = "height: 300px;",                   
+                     ggvisOutput("scatterPlot")
+                  )
+               ),  
+      
+      tabPanel("Model Summary",
+               br(), 
+               h4("R Regression model output"),                  
+               verbatimTextOutput("summary")
+              ),
+      
+      tabPanel("Language Summary",
+               br(), 
+               h4("Geographical distribution of languages"),                  
+               plotOutput("mapPlot"),
+               h4("Number of languages:", textOutput("n_languages", inline = TRUE)),
+               h4("Number of families:", textOutput("n_families", inline = TRUE))
+              )
+        )
+      )
     )
   )
-))
+)
